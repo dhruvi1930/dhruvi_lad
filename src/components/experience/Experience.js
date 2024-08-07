f89b8c97805919cd8a65d5bc2c633a0334d3cbb3
Dhruvi <dhruvihl369@gmail.com>
2024-08-07 15:17:36 -0400

    updated experience styling

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,52 +1,84 @@
 import "./style.css";
-
 function Experience() {
   return (
-    <div className="content-container">
-      <div className="top-bar-container">
-        <h1>Experience</h1>
+    <div className="experiences-container">
+      <h1>Experiences</h1>
+      <div className="developer-container">
+        <div className="title-container">
+          <h4>Full Stack Developer</h4>
+          <h4> Northern Devs, Kitchener, Ontario, Canada</h4>
+          <h5>Jan 2023 - Apr 2024 | 1.5 Years</h5>
+        </div>
+        <div className="job-description">
+          <ul>
+            <li>
+              <b>Dashboard Development:</b> Created a visually appealing
+              dashboard with interactive tools, providing clients with essential
+              business insights and KPIs (Key Performance Indicators).
+            </li>
+            <li>
+              <b>Component-Based Architecture:</b> Implemented over 15
+              TypeScript components and more than ten wireframes to design a
+              NoSQL database and component-based architecture for an admin
+              portal used in a content management system.
+            </li>
+            <li>
+              <b>E-Commerce Enhancements:</b> Integrated a merchandising payment
+              gateway and order management features, significantly enhancing
+              clients' inventory management systems.
+            </li>
+            <li>
+              <b>Client Consultations:</b> Collaborated with clients to define
+              website requirements and utilized Webflow and Figma for designing
+              and developing the website architecture, ensuring rigorous testing
+              and modifications as needed.
+            </li>
+            <li>
+              <b>Process Documentation:</b> Drafted standard processes for the
+              organization and documented all functional and unit testing
+              aspects of the projects.
+            </li>
+            <li>
+              <b>Software Development:</b> Developed, modified, and integrated
+              software code for e-commerce and other internet applications,
+              ensuring smooth operations through effective incident management.
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="container">
-        <p>
-          Northern Devs, Kitchener, Ontario, Canada <br />
-          SaaS Company | Full Stack Developer (Co-Op & Full Time)
-          <br /> Jan 2023 - Apr 2024 | 1.5 Years
-        </p>
-        Dashboard Development: Created a visually appealing dashboard with
-        interactive tools, providing clients with essential business insights
-        and KPIs (Key Performance Indicators). Component-Based Architecture:
-        Implemented over 15 TypeScript components and more than ten wireframes
-        to design a NoSQL database and component-based architecture for an admin
-        portal used in a content management system. E-Commerce Enhancements:
-        Integrated a merchandising payment gateway and order management
-        features, significantly enhancing clients' inventory management systems.
-        Client Consultations: Collaborated with clients to define website
-        requirements and utilized Webflow and Figma for designing and developing
-        the website architecture, ensuring rigorous testing and modifications as
-        needed. Process Documentation: Drafted standard processes for the
-        organization and documented all functional and unit testing aspects of
-        the projects. Software Development: Developed, modified, and integrated
-        software code for e-commerce and other internet applications, ensuring
-        smooth operations through effective incident management.
-      </div>
-      <div className="container">
-        <p>
-          Intershala, India <br />
-          Freelancer - Web & Brand Logo Design, Data Entry, Web Development
-          <br />
-          Jun 2021 - Apr 2022 | 1 Year
-        </p>
-        Logo Design: Created attractive logos using Canva, Figma, Adobe
-        Illustrator, and Photoshop, adhering to iterative feedback and client
-        branding strategies. Responsive Web Development: Developed responsive
-        websites utilizing HTML, CSS, JavaScript, and React, integrating
-        third-party APIs and ensuring comprehensive cross-browser testing.
-        Client Communication: Maintained consistent communication with clients
-        to define project scope, provide updates, and gather feedback, ensuring
-        projects met or exceeded client expectations. Project Management:
-        Efficiently managed multiple projects simultaneously, prioritizing
-        tasks, and using project management tools to track progress, maintaining
-        high standards of quality and accuracy.
+      <div className="intern-container">
+        <div className="title-container">
+          <h4>
+            Freelancer - Web & Brand Logo Design, Data Entry, Web Development
+          </h4>
+          <h4>Intershala, India</h4>
+          <h5>Jun 2021 - Apr 2022 | 1 Year</h5>
+        </div>
+        <div className="job-description">
+          <ul>
+            <li>
+              <b>Logo Design:</b> Created attractive logos using Canva, Figma,
+              Adobe Illustrator, and Photoshop, adhering to iterative feedback
+              and client branding strategies.
+            </li>
+            <li>
+              <b> Responsive Web Development:</b> Developed responsive websites
+              utilizing HTML, CSS, JavaScript, and React, integrating
+              third-party APIs and ensuring comprehensive cross-browser testing.
+            </li>
+            <li>
+              <b>Client Communication:</b> Maintained consistent communication
+              with clients to define project scope, provide updates, and gather
+              feedback, ensuring projects met or exceeded client expectations.
+            </li>
+            <li>
+              <b>Project Management:</b> Efficiently managed multiple projects
+              simultaneously, prioritizing tasks, and using project management
+              tools to track progress, maintaining high standards of quality and
+              accuracy.
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,5 +1,74 @@
+import Application_Tracker from "../../asset/Application Tracker.png";
+import ImaginifyAI from "../../asset/ImaginifyAI.png";
+import Black_Friday_Trends from "../../asset/BlackfridayTrends.png";
+import Marry_Me from "../../asset/Marry Me.png";
+import Jelly_Beans from "../../asset/Jelly Beans.png";
+import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="projects-container">
+      <h1>Projects</h1>
+      <div className="wrapper-main-container">
+        <div className="wrapper-container">
+          <img src={Application_Tracker} alt="Application Tracker" />
+          <h4>Application Tracker</h4>
+          <div className="skills-container">
+            <span>React</span>
+            <span>TypeScript</span>
+            <span>Styled-Component</span>
+            <span>Firebase</span>
+          </div>
+          <FontAwesomeIcon icon={faLink} className="link-icon" size="xs" />
+        </div>
+        <div className="wrapper-container">
+          <img src={Marry_Me} alt="Marry Me" />
+          <h4>Marry Me</h4>
+          <div className="skills-container">
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>Figma</span>
+            <span>Webflow</span>
+          </div>
+          <FontAwesomeIcon icon={faLink} className="link-icon" size="xs" />
+        </div>
+        <div className="wrapper-container">
+          <img src={Black_Friday_Trends} alt="Black Friday Trends" />
+          <h4>Black Friday Trends</h4>
+          <div className="skills-container">
+            <span>Python</span>
+            <span>jupyter notebook</span>
+            <span>Numpy</span>
+            <span>Pandas</span>
+            <span>matplotlib</span>
+          </div>
+          <FontAwesomeIcon icon={faLink} className="link-icon" size="xs" />
+        </div>
+        <div className="wrapper-container">
+          <img src={ImaginifyAI} alt="ImaginifyAI" />
+          <h4>ImaginifyAI</h4>
+          <div className="skills-container">
+            <span>React</span>
+            <span>MongoDB</span>
+            <span>Node Js</span>
+          </div>
+          <FontAwesomeIcon icon={faLink} className="link-icon" size="xs" />
+        </div>
+        <div className="wrapper-container">
+          <img src={Jelly_Beans} alt="Jelly Beans" />
+          <h4>Jelly Beans</h4>
+          <div className="skills-container">
+            <span>Vanila JavaScript</span>
+            <span>HTML</span>
+            <span>Css</span>
+          </div>
+          <FontAwesomeIcon icon={faLink} className="link-icon" size="xs" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
